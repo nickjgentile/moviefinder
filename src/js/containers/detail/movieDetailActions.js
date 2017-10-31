a0bf33cddc2e989c21903f434b77bb6e95e5cbd8
Nick Gentile <nickjgentile@gmail.com>
2017-10-31 14:24:37 -0700

    updating to https axios calls instead of http

--- src/js/containers/detail/movieDetailActions.js
+++ src/js/containers/detail/movieDetailActions.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default (id) => ({
     type: 'GET_DETAILS',
-    payload: axios.get('http://www.omdbapi.com/?apikey=8730e0e&i=' + id)
+    payload: axios.get('https://www.omdbapi.com/?apikey=8730e0e&i=' + id)
     .then(response => console.log(response) || response.data)
 })
--- src/js/containers/search/movieSearchActions.js
+++ src/js/containers/search/movieSearchActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default (search) => ({
     type: 'GET_MOVIES',
-    payload: axios.get('http://www.omdbapi.com/?apikey=8730e0e&s=' + search)
+    payload: axios.get('https://www.omdbapi.com/?apikey=8730e0e&s=' + search)
     .then((response) =>  {
         if (response.data.Error) throw new Error('Invalid Search Query');
         return response.data
